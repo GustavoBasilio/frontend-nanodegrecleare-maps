b327578cb0fc6f67efcc24bdacaa68fb04a4bd70
gusbpb@gmail.com <gusbpb@gmail.com>
2017-06-22 23:30:43 -0300

    Style of the search form

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 class Menu extends React.Component {
-    render() {   
+    render() {
         return (
             <aside id="menu-container">
-                <h1>Bart Locations</h1>
+                <h1>Near Bars</h1>
                 <div id="menu-search">
-                    <form>
-                        <input type="text" placeholder="Station Location" name="location" />
-                        <button type="submit">Filtrar</button>
+                    <form id="form-search">
+                        <input id="input-search" type="text" placeholder="Station Location" name="location" />
+                        <button id="submit-search" type="button"><i className="fa fa-filter" aria-hidden="true"></i>Filtrar</button>
                     </form>
                     <ul id="search-results">
-                        <li>teste</li>
                     </ul>
                 </div>
             </aside>

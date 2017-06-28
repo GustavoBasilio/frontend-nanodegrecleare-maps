a7131d7349490551aec943116a888afe9a1cd0b1
Gustavo Basilio <gusbpb@gmail.com>
2017-06-28 11:57:15 -0300

    Fixed open undefined page

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -28,7 +28,8 @@ module.exports = {
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
             "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
         },
-        open: true
+        open: true,
+        openPage: ""
     },
     module: {
         rules: [
